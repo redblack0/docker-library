f858bf0b0ff451511a7399974bb7ca9104ced9c5
Nick Grunert <ngredblack@gmail.com>
2022-08-08 09:10:53 +0200

    Try building interface

---
+++ include/docker/interface.hpp
@@ -0,0 +1,9 @@
+#ifndef DOCKER_LIBRARY_INTERFACE_HPP
+#define DOCKER_LIBRARY_INTERFACE_HPP
+
+#include <docker/errors.hpp>
+#include <docker/factory.hpp>
+#include <docker/container.hpp>
+#include <docker/network.hpp>
+
+#endif
