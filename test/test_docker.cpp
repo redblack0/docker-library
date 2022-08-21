55d0e7abaa98259a564469a7a29a817ecb5fb107
Nick Grunert <ngredblack@gmail.com>
2022-08-21 10:32:33 +0200

    update test

--- test/test_docker.cpp
+++ test/test_docker.cpp
@@ -15,41 +15,14 @@ int main(void) {
     std::string subnet{"10.0.0.0/8"};
     std::string ip{"10.0.0.3"};
     std::string image_name{"hello-world"};
-    //std::string image_name{"dash-docker"};
+    //Docker::Container* c;
     
     Docker::Network* n = nullptr;
-    //Docker::Container* c;
     std::string error{""};
     try {
-         n = Docker::Factory::network_create(test_network_name, subnet);
-    //     //c = Docker::Factory::container_create(image_name, n->getName(), ip);
-    //     //c->start();
-
-    //     try {
-    //         n = Docker::Factory::network_create(test_network_name, subnet);
-    //         throw std::logic_error("network name should have been taken");
-    //     } catch ( ... ) { }
-    //     // delete network -> MUST FAIL
-    //     try {
-    //         n->remove();
-    //         throw std::logic_error("remove should have failed");
-    //     } catch ( Docker::ErrorResponseFromDaemon e ) {}
-
-
-    //     // delete container and network -> SHOULD WORK
-    //     c->remove();
-    //     n->remove();
-    // } catch ( std::logic_error e ) {
-    //     error = e.what();
-    // } catch ( Docker::ErrorResponseFromDaemon e ) {
-    //     error = e.output();
-    } catch ( ... ) { }
+        n = Docker::Factory::network_create(test_network_name, subnet);
+        n->remove();
+    } catch ( ... ) { error = "error was thrown"; }
     delete n;
-    //delete c;
-
-
-    std::cout << error << std::endl;
-
-
-    // throw ...
+    if (error != "") throw std::logic_error(error);
 }
